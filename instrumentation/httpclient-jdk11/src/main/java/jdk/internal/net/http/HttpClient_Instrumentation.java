59f1b8dae8696104ce6e12d95ca09b99bb9197a2
xxia <xxia@newrelic.com>
2021-08-26 16:11:18 -0700

    instrument base class

--- instrumentation/httpclient-jdk11/src/main/java/jdk/internal/net/http/HttpClient_Instrumentation.java
+++ instrumentation/httpclient-jdk11/src/main/java/jdk/internal/net/http/HttpClient_Instrumentation.java
@@ -4,6 +4,7 @@ import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.Segment;
 import com.newrelic.api.agent.Trace;
 import com.newrelic.api.agent.Transaction;
+import com.newrelic.api.agent.weaver.MatchType;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
 import com.nr.agent.instrumentation.httpclient.Util;
@@ -15,36 +16,27 @@ import java.net.http.HttpResponse;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
-@Weave
-final class HttpClientImpl {
+@Weave(originalName = "java.net.http.HttpClient", type = MatchType.BaseClass)
+public abstract class HttpClient_Instrumentation {
 
     @Trace
-    public <T> HttpResponse<T> send(HttpRequest req, HttpResponse.BodyHandler<T> responseHandler)
-            throws IOException, InterruptedException {
-        return Weaver.callOriginal();
-    }
-
-    @Trace
-    private <T> CompletableFuture<HttpResponse<T>>
-    sendAsync(HttpRequest userRequest, HttpResponse.BodyHandler<T> responseHandler, HttpResponse.PushPromiseHandler<T> pushPromiseHandler,
-              Executor exchangeExecutor) {
+    public <T> CompletableFuture<HttpResponse<T>>
+    sendAsync(HttpRequest userRequest, HttpResponse.BodyHandler<T> responseHandler, HttpResponse.PushPromiseHandler<T> pushPromiseHandler) {
 
         URI uri = userRequest.uri();
         Segment segment = null;
         if (uri != null) {
             String scheme = uri.getScheme().toLowerCase();
-            if (scheme != null) {
-                Transaction txn = NewRelic.getAgent().getTransaction();
+            Transaction txn = NewRelic.getAgent().getTransaction();
 
-                // only instrument HTTP or HTTPS calls
-                if (("http".equals(scheme) || "https".equals(scheme)) && txn != null) {
-                    segment = txn.startSegment("javahttpclient.sendAsync");
-                }
+            // only instrument HTTP or HTTPS calls
+            if (("http".equals(scheme) || "https".equals(scheme)) && txn != null) {
+                segment = txn.startSegment("javahttpclient.sendAsync");
             }
         }
 
         CompletableFuture<HttpResponse<T>> completableFutureResponse = Weaver.callOriginal();
-        if (segment == null || uri == null) {
+        if (segment == null) {
             return completableFutureResponse;
         }
         return completableFutureResponse.whenComplete(Util.reportAsExternal(uri, segment));
